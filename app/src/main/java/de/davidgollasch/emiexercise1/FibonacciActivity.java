4771eb944e5120c6c06ea6ef1cee5c035118b84c
tomg <tom.grassmann@live.de>
2016-10-25 10:26:18 +0200

    Initial commit

---
+++ app/src/main/java/de/davidgollasch/emiexercise1/FibonacciActivity.java
@@ -0,0 +1,68 @@
+package de.davidgollasch.emiexercise1;
+
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static de.davidgollasch.emiexercise1.R.id.buttonFire;
+import static de.davidgollasch.emiexercise1.R.id.textViewFibonacci;
+
+public class FibonacciActivity extends AppCompatActivity {
+
+    private TextView tvOutput;
+    private Button btnFire;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fibonacci);
+
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.tuAkzentfarbe1BlauHell)));
+
+        InitializeActivity();
+    }
+
+    private void InitializeActivity() {
+        tvOutput = (TextView) findViewById(textViewFibonacci);
+        btnFire = (Button) findViewById(buttonFire);
+
+        btnFire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Clear text view
+                tvOutput.setText("");
+
+                // Invoke calculation
+                CalculateFibonacciRow();
+            }
+        });
+    }
+
+    /**
+     * This method has to be filled...
+     */
+    private void CalculateFibonacciRow() {
+        String output = "";
+
+        int prevPrev = 0;
+        int prev = 1;
+        int out = 0;
+
+        output = "0\n";
+
+        for (int i = 2; i <= 30; i++) {
+
+
+
+            out = prev + prevPrev;
+            output += Integer.toString(out) + "\n";
+            prevPrev = prev;
+            prev = out;
+        }
+
+        tvOutput.setText(output);
+    }
+}
